f0bcb14f004bb21b5ca8cc1504ecf5ea67e073d8
AlfieJ04 <ashlee.jones04@gmail.com>
2019-12-18 20:32:52 +0000

    Added shebang to script

--- Hack-Attack.py
+++ Hack-Attack.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python3
+
 import wx
 
 class haFrame(wx.Frame):    
     def __init__(self):
         super().__init__(parent=None, title='Hack-Attack')
+        panel = wx.Panel(self)
+        srch_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
+        srch_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+        srch_btn = wx.Button(panel, label='Search', pos=(5, 55))
+        srch_sizer.Add(srch_btn, 0, wx.ALL | wx.CENTER, 5)        
+        panel.SetSizer(srch_sizer)
+        
         self.Show()
 
 if __name__ == '__main__':
