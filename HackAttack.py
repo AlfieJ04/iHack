0c592e886132a36f06f68de4904caf4f45eae4ae
AlfieJ04 <ashlee.jones04@gmail.com>
2019-12-20 09:59:37 +0000

    Added app icon and updated HackAttack.py

--- HackAttack.py
+++ HackAttack.py
@@ -1,10 +1,34 @@
 from tkinter import *
 
 root = Tk()
+root.title("Hack-Attack")
+root.iconbitmap('.assets/img/icon.ico')
+#root.geometry("500x500+0+0")
+#frmMain = Frame(root,bg="grey")
 
-# Create a label widget
+
+# Update function
+def updateClick():
+    updateLabel = Label(root, text="Running update. Please wait")
+    updateLabel.grid(row=7, column=4)
+
+
+# Label widgets
 titleLabel = Label(root, text="Hack-Attack")
-# Show it on the screen
-titleLabel.pack()
+titleDescription = Label(root, text="The ultimate hacking tool!")
+spacerLabel = Label(root, text="        ")
+# Button widgets
+updateButton = Button(root, text="Update", padx=50, command=updateClick, bg="grey", fg="white")
+
+# Show widgets on the screen
+
+titleLabel.grid(row=1, column=4, sticky="nsew")
+titleDescription.grid(row=2, column=4, sticky="nsew")
+updateButton.grid(row=5, column=1)
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(9, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(9, weight=1)
 
 root.mainloop()
--- README.md
+++ README.md
@@ -5,16 +5,11 @@ AlfieJ04's Hack-Attack Application (Work in progress)
 A graphical user interface for running popular hacking automation scripts.
 Designed for use with Kali Linux.
 
-# Install dependencies
-
-sudo apt-get install dpkg-dev build-essential python3.7 python3.7-dev python3-pip libpython3.7-dev freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev libgstreamer-plugins-base1.0-dev libgtk-3-dev libjpeg-dev libnotify-dev libpng-dev libsdl2-dev libsm-dev libtiff-dev libwebkit2gtk-4.0-dev libxtst-dev
-
-# Clone repository
+# Download and run
 
 git clone https://github.com/AlfieJ04/Hack-Attack.git
 
 cd Hack-Attack
 
-# Install pip dependencies
+./HackAttack.py
 
-pip install -r requirements.txt
