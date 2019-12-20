a1383a42c00b77bb6190e21aeee6f9b07cee2e14
AlfieJ04 <ashlee.jones04@gmail.com>
2019-12-20 19:28:42 +0000

    Changed program name to iHack

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# Hack-Attack
+# iHack
 
-AlfieJ04's Hack-Attack Application (Work in progress)
+AlfieJ04's interactive hacking application (Work in progress)
 
 A graphical user interface for running popular hacking automation scripts.
 Designed for use with Kali Linux.
 
 # Download and run
 
-git clone https://github.com/AlfieJ04/Hack-Attack.git
+git clone https://github.com/AlfieJ04/iHack.git
 
 cd Hack-Attack
+
+pip install -r requirements.txt
 
 ./HackAttack.py
 
--- iHack.py
+++ iHack.py
@@ -1,30 +1,41 @@
 from tkinter import *
+from PIL import ImageTk, Image
+import subprocess
+
+
 
 root = Tk()
 root.title("Hack-Attack")
 root.iconbitmap('.assets/img/icon.ico')
-#root.geometry("500x500+0+0")
-#frmMain = Frame(root,bg="grey")
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.overrideredirect(1)
+root.geometry("%dx%d+0+0" % (w, h))
+root.focus_set()
+root.bind("<Escape>", lambda e: e.widget.quit())
+root.configure(background='black')
 
 
 # Update function
 def updateClick():
     updateLabel = Label(root, text="Running update. Please wait")
     updateLabel.grid(row=7, column=4)
+    subprocess.run(["sudo", "git", "clone", "https://github.com/AlfieJ04/Hack-Attack.git", "/opt/Hack-Attack/"])
 
 
 # Label widgets
 titleLabel = Label(root, text="Hack-Attack")
 titleDescription = Label(root, text="The ultimate hacking tool!")
 spacerLabel = Label(root, text="        ")
+
 # Button widgets
 updateButton = Button(root, text="Update", padx=50, command=updateClick, bg="grey", fg="white")
-
+quitButton = Button(root, text="Exit Program", command=root.quit)
 # Show widgets on the screen
 
-titleLabel.grid(row=1, column=4, sticky="nsew")
-titleDescription.grid(row=2, column=4, sticky="nsew")
+titleLabel.grid(row=2, column=4, sticky="nsew")
+titleDescription.grid(row=3, column=4, sticky="nsew")
 updateButton.grid(row=5, column=1)
+quitButton.grid(row=5, column=8)
 
 root.grid_rowconfigure(0, weight=1)
 root.grid_rowconfigure(9, weight=1)
