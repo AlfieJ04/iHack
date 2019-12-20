1fa083ee84ce1d5a4f318f09bfccaa55abc7aa75
AlfieJ04 <ashlee.jones04@gmail.com>
2019-12-20 21:52:02 +0000

    Updated iHack.py and added update.sh

--- iHack.py
+++ iHack.py
@@ -1,11 +1,12 @@
-from tkinter import *
+import os
+from tkinter import * 
+from tkinter import messagebox
 from PIL import ImageTk, Image
 import subprocess
-
-
+from git import Repo
 
 root = Tk()
-root.title("Hack-Attack")
+root.title("iHack")
 root.iconbitmap('.assets/img/icon.ico')
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 root.overrideredirect(1)
@@ -18,9 +19,17 @@ root.bind("<Escape>", lambda e: e.widget.quit())
 # Update function
 def updateClick():
     currentStatus = "Running update. Please wait"
-    subprocess.run(["sudo", "git", "clone", "https://github.com/AlfieJ04/iHack.git", "/opt/iHack/"])
-    Tk.messagebox.showinfo("Update Complete", "iHack application update completed")
+    Repo.clone_from('https://github.com/AlfieJ04/iHack.git', '/opt/iHack')
+    subprocess.call("update.sh")
+    MsgBox = messagebox.showinfo('Update Complete','All applications updated!')
 
+# Quit function
+def quitClick():
+    MsgBox = messagebox.askquestion ('Exit App','Really Quit?',icon = 'error')
+    if MsgBox == 'yes':
+       root.destroy()
+    else:
+        messagebox.showinfo('Welcome Back','Welcome back to the App')
 
 # Label widgets
 titleLabel = Label(root, text="iHack")
@@ -28,8 +37,8 @@ titleDescription = Label(root, text="The interactive hacking tool!")
 
 # Button widgets
 updateButton = Button(root, text="Update", padx=50, command=updateClick, bg="grey", fg="white")
-quitButton = Button(root, text="Exit Program", command=root.quit)
-Tk.messagebox.askyesno("Quit", "Quit?")
+quitButton = Button(root, text="Exit Program", command=quitClick, bg="grey", fg="white")
+
 # Show widgets on the screen
 
 currentStatus = ""
---
+++ update.sh
@@ -0,0 +1,291 @@
+#!/bin/bash
+
+# Global variables
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+###############################################################################################################################
+
+clear
+echo
+
+if [ -d /pentest ]; then
+     echo -e "${BLUE}Updating Discover.${NC}"
+     git pull
+     echo
+     echo
+     exit
+fi
+
+echo -e "${BLUE}Updating Kali.${NC}"
+apt-get update ; apt-get -y upgrade ; apt-get -y dist-upgrade ; apt-get -y autoremove ; apt-get -y autoclean ; echo
+
+if [ -d /opt/BloodHound/.git ]; then
+     echo -e "${BLUE}Updating BloodHound.${NC}"
+     cd /opt/BloodHound/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing BloodHound.${NC}"
+     git clone https://github.com/adaptivethreat/BloodHound.git /opt/BloodHound
+     echo
+fi
+
+if [ -d /opt/cobaltstrike-profiles/.git ]; then
+     echo -e "${BLUE}Updating Cobalt Strike profiles.${NC}"
+     cd /opt/cobaltstrike-profiles/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing Cobalt Strike profiles.${NC}"
+     git clone https://github.com/rsmudge/Malleable-C2-Profiles.git /opt/cobaltstrike-profiles
+     echo
+fi
+
+if [ -d /opt/crackmapexec/.git ]; then
+     echo -e "${BLUE}Updating CrackMapExec.${NC}"
+     cd /opt/crackmapexec/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing CrackMapExec.${NC}"
+     git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec.git /opt/crackmapexec
+     cd /opt/crackmapexec ; pip install -r requirements ; python setup.py install
+     ln -s /usr/local/bin/cme /opt/crackmapexec/crackmapexec
+     echo
+fi
+
+if [ -d /opt/discover/.git ]; then
+     echo -e "${BLUE}Updating Discover.${NC}"
+     cd /opt/discover ; git pull
+     echo
+fi
+
+if [ -d /opt/domainhunter/.git ]; then
+     echo -e "${BLUE}Updating Domain Hunter.${NC}"
+     cd /opt/domainhunter/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing Domain Hunter.${NC}"
+     git clone https://github.com/threatexpress/domainhunter.git /opt/domainhunter
+     cd /opt/domainhunter/
+     pip3 install -r requirements.txt
+     chmod 755 domainhunter.py
+     echo
+fi
+
+if [ -d /opt/DomainPasswordSpray/.git ]; then
+     echo -e "${BLUE}Updating DomainPasswordSpray.${NC}"
+     cd /opt/DomainPasswordSpray/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing DomainPasswordSpray.${NC}"
+     git clone https://github.com/dafthack/DomainPasswordSpray.git /opt/DomainPasswordSpray
+     echo
+fi
+
+if [ -d /opt/Donut/.git ]; then
+     echo -e "${BLUE}Updating Donut.${NC}"
+     cd /opt/Donut/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing Donut.${NC}"
+     git clone https://github.com/TheWover/donut.git /opt/Donut
+     echo
+fi
+
+if [ -d /opt/Egress-Assess/.git ]; then
+     echo -e "${BLUE}Updating Egress-Assess.${NC}"
+     cd /opt/Egress-Assess/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing Egress-Assess.${NC}"
+     git clone https://github.com/ChrisTruncer/Egress-Assess.git /opt/Egress-Assess
+     /opt/Egress-Assess/setup/setup.sh
+     mv server.pem ../Egress-Assess/
+     rm impacket*
+     echo
+fi
+
+if [ -d /opt/Empire/.git ]; then
+     echo -e "${BLUE}Updating Empire.${NC}"
+     cd /opt/Empire/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing Empire.${NC}"
+     git clone https://github.com/PowerShellEmpire/Empire.git /opt/Empire
+     /opt/Empire/setup/install.sh
+     echo
+fi
+
+if [ -d /opt/EyeWitness/.git ]; then
+     echo -e "${BLUE}Updating EyeWitness.${NC}"
+     cd /opt/EyeWitness/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing EyeWitness.${NC}"
+     git clone https://github.com/ChrisTruncer/EyeWitness.git /opt/EyeWitness
+     /opt/EyeWitness/setup/setup.sh
+     echo
+fi
+
+if [ ! -f /usr/bin/xmllint ]; then
+     echo -e "${YELLOW}Installing libxml2-utils.${NC}"
+     apt-get install -y libxml2-utils
+     echo
+fi
+
+echo -e "${BLUE}Updating Nmap scripts.${NC}"
+nmap --script-updatedb | egrep -v '(Starting|seconds)' | sed 's/NSE: //'
+echo
+
+if [ -d /opt/PowerSploit/docs ]; then
+     echo -e "${BLUE}Updating PowerSploit.${NC}"
+     cd /opt/PowerSploit/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing PowerSploit.${NC}"
+     rm -rf /opt/PowerSploit 2>/dev/null
+     git clone -b dev https://github.com/PowerShellMafia/PowerSploit/ /opt/PowerSploit
+echo
+fi
+
+if [ -d /opt/PowerUpSQL/.git ]; then
+     echo -e "${BLUE}Updating PowerUpSQL.${NC}"
+     cd /opt/PowerUpSQL/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing PowerUpSQL.${NC}"
+     git clone https://github.com/NetSPI/PowerUpSQL.git /opt/PowerUpSQL
+     echo
+fi
+
+if [ -d /opt/prowl/.git ]; then
+     echo -e "${BLUE}Updating Prowl.${NC}"
+     cd /opt/prowl/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing Prowl.${NC}"
+     git clone https://github.com/Pickfordmatt/Prowl /opt/prowl
+     chmod 755 /opt/prowl/prowl.py
+     apt-get install python-pip python-lxml
+     pip install dnspython Beautifulsoup4 Gitpython
+     echo
+fi
+
+if [ -d /opt/PS-Attack/.git ]; then
+     echo -e "${BLUE}Updating PS>Attack.${NC}"
+     cd /opt/PS-Attack/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing PS>Attack.${NC}"
+     git clone https://github.com/jaredhaight/PSAttack.git /opt/PS-Attack
+     echo
+fi
+
+if [ -d /opt/rawr/.git ]; then
+     echo -e "${BLUE}Updating RAWR.${NC}"
+     cd /opt/rawr/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing RAWR.${NC}"
+     git clone https://bitbucket.org/al14s/rawr.git /opt/rawr
+     /opt/rawr/install.sh y
+fi
+
+if [ -d /opt/recon-ng/.git ]; then
+     echo -e "${YELLOW}Deleting cloaned recon-ng.${NC}"
+     rm -rf /opt/recon-ng/
+     cd ~/.recon-ng/ ; rm -rf modules/ ; rm modules.yml
+     apt-get install -y python3-pyaes
+     cp -R /usr/lib/python2.7/dist-packages/PyPDF3/ /usr/lib/python3/dist-packages/
+     echo
+fi
+
+if [ ! -d $HOME/.recon-ng/modules ]; then
+     echo -e "${BLUE}Installing recon-ng modules.${NC}"
+     recon-ng -r /opt/discover/resource/recon-ng-modules-install.rc
+     echo
+fi
+
+if [ -d /opt/SharpShooter/.git ]; then
+     echo -e "${BLUE}Updating SharpShooter.${NC}"
+     cd /opt/SharpShooter/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing SharpShooter.${NC}"
+     git clone https://github.com/mdsecactivebreach/SharpShooter.git /opt/SharpShooter
+     cd /opt/SharpShooter/
+     pip install -r requirements.txt
+     echo
+fi
+
+if [ -d /opt/theHarvester/.git ]; then
+     echo -e "${BLUE}Updating theHarvester.${NC}"
+     cd /opt/theHarvester/ ; git pull
+     python3 -m pip install -r requirements.txt | grep -v 'already satisfied'
+     echo
+else
+     echo -e "${YELLOW}Installing theHarvester.${NC}"
+     git clone https://github.com/laramies/theHarvester.git /opt/theHarvester
+     cd /opt/theHarvester
+     python3 -m pip install -r requirements.txt
+     echo
+fi
+
+if [ ! -e /usr/lib/python3/dist-packages/texttable.py ]; then
+     echo -e "${YELLOW}Installing Texttable.${NC}"
+     apt-get install -y python3-texttable
+     echo
+fi
+
+if [ -d /opt/unicorn/.git ]; then
+     echo -e "${BLUE}Updating unicorn.${NC}"
+     cd /opt/unicorn/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing unicorn.${NC}"
+     git clone https://github.com/trustedsec/unicorn.git /opt/unicorn
+     echo
+fi
+
+if [ -d /opt/Veil/.git ]; then
+     echo -e "${BLUE}Updating Veil.${NC}"
+     cd /opt/Veil/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing Veil.${NC}"
+     git clone https://github.com/Veil-Framework/Veil /opt/Veil
+     ./opt/Veil/config/setup.sh --force --silent
+     echo
+fi
+if [ -d /opt/spiderfoot/.git ]; then
+     echo -e "${BLUE}Updating spiderfoot.${NC}"
+     cd /opt/unicorn/ ; git pull
+     echo
+else
+     echo -e "${YELLOW}Installing spiderfoot.${NC}"
+     git clone https://github.com/smicallef/spiderfoot.git /opt/spiderfoot
+     echo
+fi
+if [ ! -f /usr/bin/xdotool ]; then
+     echo -e "${YELLOW}Installing xdotool.${NC}"
+     apt-get install -y xdotool
+     echo
+fi
+
+if [ ! -f /usr/bin/xlsx2csv ]; then
+     echo -e "${YELLOW}Installing xlsx2csv.${NC}"
+     apt-get install -y xlsx2csv
+     echo
+fi
+
+if [ ! -f /usr/bin/xml_grep ]; then
+     echo -e "${YELLOW}Installing xml_grep.${NC}"
+     apt-get install -y xml-twig-tools
+     echo
+fi
+
+echo -e "${BLUE}Updating locate database.${NC}" ; updatedb
+
+echo
+echo
+exit
