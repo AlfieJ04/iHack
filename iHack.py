e9561713d055db47480cdca15f114e886b91acf0
alfiej04 <ashlee.jones04@gmail.com>
2019-12-23 15:17:28 +0000

    Added console to GUI

--- iHack.py
+++ iHack.py
@@ -19,16 +19,14 @@ Created by AlfieJ04
 import os
 import time
 import threading
-import tkinter.font
 from tkinter import *
 from tkinter import messagebox
 from tkinter import Button, Tk, HORIZONTAL
+import tkinter.font
 from PIL import ImageTk, Image
 import subprocess
 #from git import Repo
 
-# Set currentStatus
-currentStatus = "Waiting for input"
 
 ###################################################
 #                                                 #
@@ -39,18 +37,27 @@ currentStatus = "Waiting for input"
 # Create and configure root window
 root = Tk()
 root.title("iHack")
-root.iconbitmap('.assets/img/logo.gif')
+root.iconbitmap(r'.assets/img/logo.png')
+
+# Create the menubar
+menubar = Menu(root)
+root.config(menu=menubar)
+
+# Create the submenu
+subMenu = Menu(menubar, tearoff=0)
+
 #w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 #root.overrideredirect(1)
 #root.geometry("%dx%d+0+0" % (w, h))
 root.focus_set()
 root.bind("<Escape>", lambda e: e.widget.quit())
 #root.configure(background='black')
-titleFont = tkinter.font.Font(family = 'Helvetica', size = 12, weight = "bold")
+titleFont = tkinter.font.Font(family = 'Verdana', size = 12, weight = "bold")
 # Create main frame
 
 mainFrame = LabelFrame(root, padx=5, pady=5)
 mainFrame.pack()
+
 ###################################################
 #                                                 #
 #                    FUNCTIONS                    #
@@ -58,21 +65,23 @@ mainFrame.pack()
 ###################################################
 
 
+def get_info(arg):
+    print (tfield.get("1.0", "current lineend"))
+
 # Update function
 def updateClick():
-    currentStatus = "Running update. Please wait"
-    root.update()
+    statusLabel["text"] = "Running update. Please wait"
     gitCommand = "cd /opt/iHack && git pull"
     process = subprocess.Popen(gitCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     MsgBox = messagebox.showinfo('Update Complete','iHack updated!')
+    statusLabel["text"] = "Update completed"
 
 # Install function
 def installClick():
     MsgBox = messagebox.askquestion ('Install Applications','Are you sure you want to install all applications?',icon = 'warning')
     if MsgBox == 'yes':
-        currentStatus = "Installing applications. Please wait"
-        root.update()
+        statusLabel["text"] = "Installing applications. Please wait"
         subprocess.call("update.sh")
         MsgBox = messagebox.showinfo('Install Complete','All applications installed!')
         
@@ -81,12 +90,32 @@ def installClick():
 
 # Quit function
 def quitClick():
-    currentStatus = "Quitting"
+    statusLabel["text"] = "Quitting"
     MsgBox = messagebox.askquestion ('Exit App','Really Quit?',icon = 'error')
     if MsgBox == 'yes':
        root.destroy()
     else:
         messagebox.showinfo('Welcome Back','Welcome back to the App')
+
+
+###################################################
+#                                                 #
+#                    MENU BUTTONS                 #
+#                                                 #
+###################################################
+
+menubar.add_cascade(label="File", menu=subMenu)
+subMenu.add_command(label="Install", command=updateClick)
+subMenu.add_command(label="Exit", command=root.destroy)
+
+
+def about_us():
+    tkinter.messagebox.showinfo('About iHack', 'iHack is an interactive automated hacking application built using Python Tkinter by @AlfieJ04')
+
+
+subMenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Help", menu=subMenu)
+subMenu.add_command(label="About Us", command=about_us)
 
 
 ###################################################
@@ -107,12 +136,31 @@ quitButton = Button(mainFrame, padx=50, text="Exit Program", command=quitClick, 
 
 ###################################################
 #                                                 #
+#                 TERMINAL BOX                    #
+#                                                 #
+###################################################
+
+termf = Frame(root, height=400, width=500, bg="black")
+termf.pack(fill=BOTH, expand=YES)
+tfield = Text(root)
+f = os.popen("cd /opt/iHack && git pull")
+for line in f:
+    line = line.strip()
+    if line:
+        tfield.insert("end", line+"\n")
+        # tfield.get("current linestart", "current lineend")
+tfield.bind("<Return>", get_info)
+#wid = termf.winfo_id()
+#os.system('xterm -into %d -geometry 40x20 -sb &' % wid)
+
+###################################################
+#                                                 #
 #                   STATUS BAR                    #
 #                                                 #
 ###################################################
 
 # Status Bar
-statusLabel = Label(root, text=str(currentStatus), bd=1, relief=SUNKEN, anchor=E, bg="grey", fg="white", font=titleFont)
+statusLabel = Label(root, text="Welcome to iHack", bd=1, relief=SUNKEN, anchor=E, bg="grey", fg="white", font=titleFont)
 #statusLabel.grid(row=8, column=1, columnspan=7, sticky=W+E)
 statusLabel.pack(side=BOTTOM, fill=X, expand=1, pady=10)
 
@@ -157,3 +205,4 @@ quitButton.pack(side=LEFT)
 ###################################################
 
 root.mainloop()
+f.close()
